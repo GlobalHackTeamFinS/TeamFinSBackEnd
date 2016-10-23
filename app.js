70225f16ab0d734c6eb8388583ef5afef56aadb7
Kohl Kohlbrenner <kkohlbrenner@STL-KKohlbrenner.local>
2016-10-23 02:48:54 -0500

    changing routing

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ app.get('/add', authenticate, providerController.add);*/
  * Provider App Routes
  */
 
- app.get('/', homeController.index);
+ app.get('/provider/nearby', homeController.index);
  // app.get('/dashboard', homeController.dashboard);
  app.post('/provider/new', providerController.newProvider);
  app.put('/provider/:id', authenticate, providerController.updateProvider);
